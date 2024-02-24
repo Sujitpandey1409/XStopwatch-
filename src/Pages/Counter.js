8a4a2b352af770d03d9e53cc2efd1899fb393b74
Sujitpandey1409 <sujitpandey1409@gmail.com>
2024-02-24 22:57:14 +0530

    counter App Added with css Styling

--- src/Pages/Counter.js
+++ src/Pages/Counter.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 const Counter = () => {
     const[countVal, setCountVal] = useState(0)
     return ( 
-        <div>
-            <h1>Count App</h1>
+        <div className='CounterContainer'>
+            <h1>Counter App</h1>
             <p>Count: {countVal}</p>
             <div className='buttonContainer'>
                 <button onClick={()=>{setCountVal((countVal)=>countVal+1)}}>Increment</button>
