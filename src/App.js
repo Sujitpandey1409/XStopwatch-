949134801809968a14a32ce747372649a4d9f6cb
Sujitpandey1409 <sujitpandey1409@gmail.com>
2024-02-24 19:39:36 +0530

    added webpage for fullName diplay miniproject with navbar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Stopwatch from './Components/Stopwatch';
 import FullNameDisplay from "./Pages/FullNameDisplay";
+import Nav from "./Components/Navbar";
 
 function App() {
   return (
+    <>
+      <Nav />
     <div className="App">
       <Routes>
         <Route path='/' element={<Stopwatch />}/>
         <Route path='/displayName' element={<FullNameDisplay />}/>
       </Routes>
     </div>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css'
+const Nav = () => {
+  const Navigate = useNavigate()  
+  return (
+    <div className="navbar">
+      <p onClick={()=>{Navigate('/')}} >Stopwatch</p>
+      <p onClick={()=>{Navigate('/displayName')}} >DisplayName</p>
+    </div>
+  );
+};
+
+export default Nav;
