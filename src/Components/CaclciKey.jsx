5f8ca84b001dace09c8cfb2ee16c92edda4e6b6a
Sujitpandey1409 <sujitpandey1409@gmail.com>
2024-02-26 20:53:48 +0530

    added XStates axios

--- src/Components/CaclciKey.jsx
+++ src/Components/CaclciKey.jsx
@@ -5,15 +5,6 @@ const CaclciKey = ({keys, updateInput, showResult}) => {
     const buttonVal = useRef(null)
     const [inputDataStr, setInputDataStr] = useState('')
     let operator = '+-*/'
-    // const handleClick = ()=>{
-    //     // let keys = buttonVal.current.textContent
-    //     console.log(keys, keys==='=', keys==='C');
-    //     console.log(inputDataStr)
-    //     if (!(operator.includes(keys)&&inputDataStr[inputDataStr.length-1]===keys)){
-    //     if(keys === 'C'){console.log('C clicked',inputDataStr); setInputDataStr((inputDataStr)=> inputDataStr=''); updateInput('')}
-    //     else if(buttonVal.current.textContent === '='){console.log('= clicked'); inputDataStr&&showResult(eval(inputDataStr))}
-    //     else{setInputDataStr((inputDataStr)=>inputDataStr+keys); console.log(inputDataStr+keys); updateInput(inputDataStr+keys)}}
-    // }
     return ( <button ref={buttonVal} className='key'>{keys}</button> );
 }
  
