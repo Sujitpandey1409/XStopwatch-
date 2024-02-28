e49a14f00e3062e801296e206c697849c20f262f
Sujitpandey1409 <sujitpandey1409@gmail.com>
2024-02-28 10:01:01 +0530

    added weather App with styling bug fixes

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './WeatherCard.css'
 const WeatherCard = ({title, value}) => {
     return ( 
-        <div className="weather-card">
+        <div className="weather-cards">
             <h4>{title}</h4>
             <p>{value}</p>
         </div>
